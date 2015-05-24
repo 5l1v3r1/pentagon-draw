770fd147c0d1da6728c9632d6b7bc32f32413b56
Alex Nichol <unixpickle@gmail.com>
2015-05-24 12:17:58 -0400

    traces canvas

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-var PENTAGON_SIZE = 0.05;
+var PENTAGON_SIZE = 0.025;
 var MOVE_DELAY = 1000;
 var PENTAGON_IMAGE_SIZE = 100;
 
-function Pentagon(x, y) {
+var LOWER_ALPHA_THRESHOLD = 0.01;
+var UPPER_ALPHA_THRESHOLD = 0.8;
+
+function Pentagon(x, y, alpha) {
   this.x = x;
   this.y = y;
+  this.alpha = alpha;
   this.distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 }
 
@@ -24,7 +28,7 @@ function ScreenLayout(pentagons) {
   this.startTime = new Date().getTime();
   var pentagonSize = Math.round(PENTAGON_SIZE * this.screenSize);
   var pentagonImage = pentagonImageURL();
-  for (var i = pentagons.length; i > 0; --i) {
+  for (var i = pentagons.length-1; i >= 0; --i) {
     var element = document.createElement('img');
     element.src = pentagonImageURL();
     element.width = pentagonSize;
@@ -35,6 +39,7 @@ function ScreenLayout(pentagons) {
     element.style.top = Math.round(window.innerHeight/2 - pentagonSize/2) +
       'px';
     element.style.webkitTransform = 'translate3d(0, 0, 0) rotate(0deg)';
+    element.style.opacity = pentagons[i].alpha.toFixed(5);
     document.body.appendChild(element);
     this.elements.push(element);
   }
@@ -50,7 +55,7 @@ ScreenLayout.prototype.requestAnimationFrame = function() {
 
 ScreenLayout.prototype.tick = function() {
   var elapsed = new Date().getTime() - this.startTime;
-  var distance = Math.pow(Math.sin(Math.max(elapsed - MOVE_DELAY, 0) / 2000), 2)*1.25;
+  var distance = Math.pow(Math.sin(Math.max(elapsed - MOVE_DELAY, 0) / 2000), 2)*Math.sqrt(2);
   var angle = (elapsed / 2) % 360;
   for (var i = 0, len = this.elements.length; i < len; ++i) {
     var pentagon = this.pentagons[i];
@@ -70,9 +75,9 @@ function pentagonImageURL() {
   var canvas = document.createElement('canvas');
   canvas.width = PENTAGON_IMAGE_SIZE;
   canvas.height = PENTAGON_IMAGE_SIZE;
-  
+
   var context = canvas.getContext('2d');
-  context.fillStyle = 'rgba(255, 255, 255, 0.5)';
+  context.fillStyle = 'white';
   context.beginPath();
   for (var angle = 0; angle < 360; angle += 360/5) {
     var x = Math.cos(angle * Math.PI / 180)*PENTAGON_IMAGE_SIZE/2 +
@@ -87,16 +92,53 @@ function pentagonImageURL() {
   }
   context.closePath();
   context.fill();
-  
+
   return canvas.toDataURL('image/png');
 }
 
-window.addEventListener('load', function() {
+function pentagonsForCanvas(canvas, context) {
+  var size = canvas.width;
+  var imageData = context.getImageData(0, 0, size, size);
+  var sampleSize = PENTAGON_SIZE * size;
   var pentagons = [];
-  for (var x = -0.9; x <= 0.9; x += 0.3) {
-    for (var y = -0.9; y <= 0.9; y += 0.3) {
-      pentagons.push(new Pentagon(x, y));
+  for (var i = 0; i < 1; i += PENTAGON_SIZE) {
+    var x = Math.floor(size * i);
+    for (var j = 0; j < 1; j += PENTAGON_SIZE) {
+      var y = Math.floor(size * j);
+      var avg = averageAlpha(imageData, x, y, Math.floor(sampleSize));
+      if (avg < LOWER_ALPHA_THRESHOLD) {
+        continue;
+      } else if (avg > UPPER_ALPHA_THRESHOLD) {
+        avg = UPPER_ALPHA_THRESHOLD;
+      }
+      var scaledX = 2*(x+sampleSize/2)/size - 1;
+      var scaledY = 2*(y+sampleSize/2)/size - 1;
+      pentagons.push(new Pentagon(scaledX, scaledY, avg));
     }
   }
-  new ScreenLayout(pentagons).tick();
+  return pentagons;
+}
+
+function averageAlpha(imageData, startX, startY, size) {
+  var sum = 0;
+  for (var x = startX; x < startX + size; ++x) {
+    for (var y = startY; y < startY + size; ++y) {
+      var index = 4 * (x+y*imageData.width);
+      sum += imageData.data[index + 3] / 255;
+    }
+  }
+  return sum / (size * size);
+}
+
+window.addEventListener('load', function() {
+  var canvas = document.createElement('canvas');
+  canvas.width = 200;
+  canvas.height = 200;
+
+  var context = canvas.getContext('2d');
+  context.fillStyle = 'white';
+  context.font = '140px sans-serif';
+  context.fillText('Hi', 35, 150);
+
+  new ScreenLayout(pentagonsForCanvas(canvas, context)).tick();
 });
